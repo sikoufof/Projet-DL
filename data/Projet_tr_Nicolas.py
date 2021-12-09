4084e53a0b089dc2cf310e878693f3e86127024c
sikoufof <sikou.fofana01@etu.umontpellier>
2021-12-09 20:01:47 +0100

    inutile

--- data/Projet_tr_Nicolas.py
+++ data/Projet_tr_Nicolas.py
@@ -1,69 +1,73 @@
 #%%
-from inspect import Traceback
+from networkx.algorithms.shortest_paths.weighted import dijkstra_path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import itertools
-import folium 
-import openrouteservice 
-import json
-
-from openrouteservice import convert
-from networkx.algorithms.shortest_paths.weighted import dijkstra_path
 from itertools import combinations
 from functools import partial
 from pyproj import CRS
 from pyproj import Proj, transform
+
+# widget manipulation
 from ipywidgets import widgets, interact, interactive, fixed, interact_manual
-from download import download 
+
+from download import download  # download data / avoid re-downloading
 from IPython import get_ipython
 
 pd.options.display.max_rows = 8
 
 #%%
-data_co = pd.read_csv("coordonnees.csv", sep = ',')
-
 #La colonne : Nom gare a été remplacé par NomGare dans le csv de base, y'a des espaces qui traines
 dta1 = pd.read_csv("gares-peage-2019.csv", sep = ';')
+dta1.rename({' NomGare ':'NomGare'},axis=1,inplace=True)
 
-dta1.rename({' NomGare ':'NOMGARE'},axis=1,inplace=True)
-dta1.columns = map(lambda x: str(x).upper(), dta1.columns)
 
+data_co = pd.read_csv("coordonnees.csv", sep = ',')
+
+price = pd.read_csv("DataFrame_price.csv", sep=';')
+price = price.fillna(0)
+
+price.columns = ([0]+list(data_co.NOMGARE))
+price.index = list(data_co.NOMGARE)
+
+#price.columns = map(lambda x: str(x).upper(), price.columns)
+#a = list(price.iloc[:,0])
+#price.index = a
+#price.index = map(lambda x: str(x).upper(), price.index)
+
+#%%
+
+#Création du tableau des distances
 
 #il doit y avoir une valeur x et y dans le csv sous forme de string:'2' à la place de seulement 2, ex dta1.loc[0,'x']='2'
 for i in range(len(dta1.index)):
-    dta1.loc[i,'X']=float(dta1.loc[i,'X'].replace(',','.'))
-    dta1.loc[i,'Y']=float(dta1.loc[i,'Y'].replace(',','.'))
+    dta1.loc[i,'x']=float(dta1.loc[i,'x'].replace(',','.'))
+    dta1.loc[i,'y']=float(dta1.loc[i,'y'].replace(',','.'))
 
 #Extraction des données relatives aux autoroutes A9, A709, A61, A62, A75 et A66
-dta_routes = dta1[(dta1.ROUTE=="A0009")|(dta1.ROUTE=="A0709")|(dta1.ROUTE=="A0061")|(dta1.ROUTE=="A0062")|(dta1.ROUTE=="A0075")|(dta1.ROUTE=="A0066")]  
-dta_routes = dta_routes[['ROUTE','NOMGARE','X','Y']]
+dta_routes = dta1[(dta1.route=="A0009")|(dta1.route=="A0709")|(dta1.route=="A0061")|(dta1.route=="A0062")|(dta1.route=="A0075")|(dta1.route=="A0066")]  
+dta_routes = dta_routes[['route','NomGare','x','y']]
+# a été rectifié : dta_routes= dta_routes.iloc[:,[1,13,7,8]] #pb sur dta1.Nom gare / dta1['Nom gare'] / dta1[['Nom gare']]
 
 # reafections des indices
 dta_routes.reset_index(drop = True, inplace = True)
-
-for i in range(len(dta_routes.NOMGARE)):
-    if dta_routes.loc[i,'NOMGARE'] in list(data_co.NOMGARE):
-        print(dta_routes.loc[i,'NOMGARE'])
-
-#On constate que le csv ne contient pas toute les sorties disponible,
-#Il faut donc aller chercher les sorties sur le second csv donnée en lien,
-#i.e le csv trace-du-reseau-autoroutier-doccitanie.
-
-#Continuons tout de même la démarche jusqu'à la créations du tableau 
-#contenant les coordonnées gps des villes selectionnées.
-
-for i in range(len(dta_routes.NOMGARE)):
-    if (dta_routes.loc[i,'NOMGARE'] not in list(data_co.NOMGARE)):
-        dta_routes.drop(i,inplace=True)
-
-dta_routes.reset_index(drop = True, inplace = True)
+dta_routes
 
 #transformation des coordonnées Lambert93 en coordonéees GPS
+from pyproj import Proj, transform
 
-X = dta_routes['X']
-Y = dta_routes['Y']
+inProj = Proj(init='epsg:2154')
+outProj = Proj(init='epsg:4326')
+x1= 702805
+y1 = 6230817.3
+x2,y2 = transform(inProj,outProj,x1,y1)
+print(x2,y2)
+702805,6230817
+
+X = dta_routes['x']
+Y = dta_routes['y']
 inProj = Proj(init='epsg:2154')
 outProj = Proj(init='epsg:4326')
 GPS=[]
@@ -73,69 +77,28 @@ for i in range(len(dta_routes.index)):
     GPS.append(transform(inProj,outProj,X[i],Y[i]))
 GPS
 for i in range(len(GPS)):
-    dta_routes.loc[i,'X'],dta_routes.loc[i,'Y']=GPS[i]
+    dta_routes.loc[i,'x'],dta_routes.loc[i,'y']=GPS[i]
 dta_routes
+
+#On garde les routes nous concernant, on supprime donc les autres
+dta_routes = dta_routes.drop(0)
+dta_routes = dta_routes.drop(1)
+dta_routes = dta_routes.drop(2)
+dta_routes = dta_routes.drop(3)
+dta_routes = dta_routes.drop(5)
+dta_routes = dta_routes.drop(6)
+dta_routes = dta_routes.drop(7)
+dta_routes = dta_routes.drop(18)
+dta_routes = dta_routes.drop(31)
+dta_routes = dta_routes.drop(32)
+dta_routes = dta_routes.drop(33)
+dta_routes = dta_routes.drop(34)
+dta_routes = dta_routes.drop(35)
+# il en reste à supprimer, mais on utilise coordonnées 
 
 #Transformation en fichier .csv
 dta_routes.to_csv('routes.csv', sep = ';')
 
-# Le csv gares-peage-2019 ne contient pas toutes les routes de notre étude, on peut aller les prendre
-# le csv trace-du-reseau-autoroutier-doccitanie.
-
-
-#%%
-#Effectuons la même démarche pour le second csv.
-
-dta2 = pd.read_csv("trace-du-reseau-autoroutier-doccitanie.csv", sep = ';')
-
-dta_routes2 = dta2[(dta2.nom_route=="A9")|(dta2.nom_route=="A709")|(dta2.nom_route=="A61")|(dta2.nom_route=="A62")|(dta2.nom_route=="A75")|(dta2.nom_route=="A66")]  
-
-#On peut remarquer que les noms et les numéros de sorties ne sont pas indiqués
-#Par soucis de temps on décide donc d'utiliser le csv data_co de l'un de nos camarades.
-#%%
-
-price = pd.read_csv("DataFrame_price.csv", sep=';')
-price = price.fillna(0)
-
-price.columns = ([0]+list(data_co.NOMGARE))
-price.index = list(data_co.NOMGARE)
-del price[0]
-#%%
-#On utilise pour la suite le tableau obtenue précédement, cependant pour plus de lisibilité
-#nettoyons légèrement le tableau des prix, en excluant les sorties de péage nulle.
-
-price2 = price
-del price2['VENDARGUES']
-del price2['MONTPELLIER EST']
-del price2['MONTPELLIER SUD']
-del price2['MONTPELLIER OUEST']
-del price2['ST JEAN DE VEDAS']
-del price2['MONTPELLIER']
-del price2['LE BOULOU (OUVERT)']
-del price2['PEAGE TOULOUSE SUD/OUEST']
-del price2['LE PALAYS']
-del price2['PEAGE TOULOUSE SUD/EST']
-del price2['MONTAUDRAN']
-del price2['LASBORDES']
-del price2['SOUPETARD']
-del price2['LA ROSERAIE']
-del price2['LA CROIX DAURADE']
-del price2['BORDEROUGE']
-del price2['LES IZARDS']
-del price2['SESQUIERES']
-
-for i in ['VENDARGUES','MONTPELLIER EST','MONTPELLIER SUD','MONTPELLIER OUEST','ST JEAN DE VEDAS',
-'MONTPELLIER','LE BOULOU (OUVERT)','PEAGE TOULOUSE SUD/OUEST','LE PALAYS','PEAGE TOULOUSE SUD/EST',
-'MONTAUDRAN','LASBORDES','SOUPETARD','LA ROSERAIE','LA CROIX DAURADE','BORDEROUGE','LES IZARDS',
-'SESQUIERES']:
-    price2.drop(i,inplace=True)
-
-
-
-#Création de la base de données finale qui contient le prix des péages que nous avons retenus
-price2.to_csv('DataFrame_price2.csv')
-
-#%%
 
 #Créations des portions de routes en 5.
 Sorties=[0,1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,19,
@@ -198,11 +161,9 @@ def chemin(e,s):
     s = transforme(s)
     i_e = id_portion(e)
     i_s = id_portion(s)
-    min_pe = position_portion(min(P[i_e][0])) 
-    # = 0
+    min_pe = position_portion(min(P[i_e][0])) # = 0
     max_pe = position_portion(max(P[i_e][0]))
-    min_ps = position_portion(min(P[i_s][0])) 
-    # = 0
+    min_ps = position_portion(min(P[i_s][0])) # = 0
     max_ps = position_portion(max(P[i_s][0]))
     pp_e = position_portion(e)
     pp_s = position_portion(s)
@@ -254,19 +215,12 @@ def cout_direct(e,s):
     '''retourne le coût d'un allé direct entre entré/sortie
     (e/s sous forme int/string)'''
     e = transforme(e)
-    s = transforme(s)
-    prix = float(price.iloc[e,s])     
-    # les éléments du tableau price sont des numpy.float et non des float 
-    # pause problème lors d'opérations
+    s = transforme(s)+1
+    prix = price.iloc[e,s]
     return(prix)
 
-#NARBONNE SUD valeur  = 8.58,5 
-price.loc['VENDARGUES','NARBONNE SUD'] = '8.585'
 
-
-def chemin_k_sortie(e,s,k):
-    e = transforme(e)
-    s = transforme(s)
+def prix_chemin(e,s,k):
     L = trajet(e,s,k)
     long_L = len(L)
     L2 = [0]*long_L
@@ -283,86 +237,122 @@ def chemin_k_sortie(e,s,k):
         L2[i] = prix_c
     return([L[indice],L2[indice]])
 
-def chemin_opt(e,s,k):
-    if e==s:
-        return("Vous devez choisir une sortie différente de votre point d'entrée")
-    opt = chemin_k_sortie(e,s,0)
-    for i in range(1,k+1):
-        opt_c = chemin_k_sortie(e,s,i)
-        if opt_c[1] < opt[1]:
-            opt = opt_c 
-    for i in range(len(opt[0])):
-        opt[0][i] = re_transforme(opt[0][i])
-    return(opt)
-
-def nb_sortie_possible(e,s):
-    return(len(chemin(e,s))-2)
 
 
-# carte interactive (Classe Graph)
+def prixopt(A):
+    L=[]
+    somm1=1000
+    somm2=0
+    for i in range(len(A)):
+        for j in range(len(A[i])-1):
+            somm2+=price.iloc[A[i][j],A[i][j+1]+1]
+        if somm2<somm1 :
+            somm1=somm2
+            somm2=0
+            L=A[i]
+        else:
+            somm2=0
+    return [L,somm1]
 
-class Graph(object):
-    def __init__(self):
-        pass
+#%%
+def Finale(A,B,k):
+    L=[]
+    if A==B:
+        return "Vous devez choisir une sortie différente de votre point d'entrée"
+    for i in range(k+1):
+        L.append(prixopt(trajet(A,B,i)))
+    a=prixopt(trajet(A,B,0))
+    for j in range(len(L)):
+        if L[j][1]<a[1]:
+            a=L[j]
+    a.append(len(a[0])-2)
+    return a
 
-    def carte(self,DEPART,ARRIVEE):
 
-        client = openrouteservice.Client(key='5b3ce3597851110001cf62486f5564a064e34f3895221e5a0d9a2405')
-    
-        ligne_DEPART = data_co[data_co.NOMGARE == DEPART].index[0]
-        ligne_ARRIVEE = data_co[data_co.NOMGARE == ARRIVEE].index[0]
 
-        x = [data_co.loc[ligne_DEPART,'X'],data_co.loc[ligne_ARRIVEE,'X']]
-        y = [data_co.loc[ligne_DEPART,'Y'],data_co.loc[ligne_ARRIVEE,'Y']]
-    
-        m = folium.Map(
-            location=[43.1838000,3.0050000],
-            zoom_start=10, 
-            control_scale=True)
 
-        coords  = (x[0],y[0]),(x[1],y[1])
-        res = client.directions(coords)
 
-        geometry = client.directions(coords)['routes'][0]['geometry'] 
-        decoded = convert.decode_polyline(geometry) 
+def sortie_moins_cher(LB):
+    '''LB est une liste de string (chemin)'''
+    long_LB = len(LB)
+    if (long_LB == 2) | (long_LB == 1) | (long_LB == 0) :
+        return("aucune sortie")
+    indice = 1
+    prix = cout_direct(LB[0],LB[1])
+    for i in range(2,long_LB):
+        prix_2 = cout_direct(LB[0],LB[i])
+        if prix > prix_2:
+            prix = prix_2
+            indice = i
+    return([prix,LB[indice]])
 
-        distance_txt = "<h4> <b>Distance : " + "<strong>"+str(round(res['routes'][0]['summary']['distance']/1000,1))+ " Km </strong>" +"</h4></b>"
-        duration_txt = "<h4> <b>Duration : " + "<strong>"+str(round(res['routes'][0]['summary']['duration']/60,1))+ " Min. </strong>" +"</h4></b>"
 
-        folium.GeoJson(decoded).add_child(folium.Popup(distance_txt+duration_txt,max_width=300)).add_to(m)
 
-        folium.Marker(
-            location=list(coords[0][::-1]),
-            popup="DEPART",
-            icon=folium.Icon(color="green"),
-            ).add_to(m)
+#%%
+def chemin_k_sortie(k,e,s):
+    ch = chemin(e,s)
+    long_ch = len(ch)
+    sortie = 0
+    if (long_ch == 2) :
+        return("faites un allé direct")
+    if (long_ch == 1) | (long_ch == 0) :
+        return("aucune entré/sortie indiqué")
+    prix_compare = [ [], [ [],[] ] ]
+    for i in range(1,len(ch)):
+        prix_compare[0].append(cout_direct(e,i)[0])
+        prix_compare[1][0].append(transforme(cout_direct(e,i)[1]))
+        prix_compare[1][1].append(sortie)
 
-        folium.Marker(
-            location=list(coords[1][::-1]),
-            popup="ARRIVEE",
-            icon=folium.Icon(color="red"),
-            ).add_to(m)
+def dijkstra(e,s,k):
+    ch = chemin(e,s)
+    long_ch = len(ch)
+    if (long_ch == 2) :
+        return("faites un allé direct")
+    if (long_ch == 1) | (long_ch == 0) :
+        return("aucune entré/sortie indiqué")
+    liste_op = [e]
+    k_c = k
+    n = 0
+    chemin_co = chemin(e,s)
+    prix = 0
+    l_prix = [[0,0]]*k
+    while (n!=300) | (k_c != 0) | (len(chemin_co) != 2):
+        prix_co,sortie_EV = sortie_moins_cher(chemin_co)
+        prix_cu = prix_co + prix
+        if prix_cu <= min(l_prix):
 
-        return(m)
 
-#interface 
+        dijkstra_etape(k_c,)
+    return(liste_op)
 
-#selection du tableau des noms des villes nous intéressant!
 
-villes_interface = data_co.NOMGARE
-for i in [5,17,18,32,34]:
-    villes_interface = villes_interface.drop(i)
-villes_interface = list(villes_interface)
+def dijkstra_etape(k,trajet,list_p):
 
-def interface_carte(DEPART,ARRIVEE,k):
-    a = nb_sortie_possible(DEPART,ARRIVEE)
-    if k > a:
-        return('le nombre de sortie maximum est', a,'sortie(s)' )
-    a = chemin_opt(DEPART,ARRIVEE,k)
-    print('le trajet le moins cher pour', k, 'sortie est le trajet', a[0],'il vous coûtera', a[1],'Euros')
-    b = Graph()
-    b = b.carte(DEPART,ARRIVEE)
-    return(b)
+    prix_p = P_S[0][0]
+    k_p = k
+    if k_p != 0: 
+        a = sortie_moins_cher(chemin(P_S[1],s))
+        prix_c = a[0]
+        prix_cu = prix_p + prix_c
+        if prix_cu <= min(P_S[0][1:]):
+            k_p = k_p - 1
+            up_grade(P_S,prix_cu,)
 
-interact(interface_carte,DEPART=villes_interface,ARRIVEE=villes_interface,k=(0,25))
+
+
+
+    a = sortie_moins_cher(list)
+    b = sortie_moins_cher(list[a[1]:])#à rectifier 
+    prix_mi_tr = a[0]+b[0]
+    del a[0][0]
+
+    if prix_mi_tr <= min(a[0]):
+        return()
+    l = sorted(list)
+    p = list[0]
+    i = 0
+    for i in range(len(l)):
+        if p < list[i+1]:
+
+# carte interactive 
 
